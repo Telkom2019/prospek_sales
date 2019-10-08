544e1490ab31cdeccc03cdfde2b6f6953b30ec82
imronsmdrj <imronsumadireza@gmail.com>
2019-10-08 09:41:14 +0700

    upload data

---
+++ application/controllers/C_mhs.php
@@ -0,0 +1,12 @@
+<?php 
+class C_mhs extends CI_Controller{
+
+	public function index (){
+		$this->load->model('m_mhs');
+		$data['mahasiswa']=$this->m_mhs->get_data();
+
+		$this->load->view('v_mhs', $data);
+	}
+}
+
+ ?>
---
+++ application/views/dashboard.php
@@ -0,0 +1,223 @@
+
+
+  <!-- Left side column. contains the logo and sidebar -->
+  
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+
+      <div class="row">
+         <div class="col-md-12 col-sm-10 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-red"><i class="fa fa-database"></i></span>
+
+            <div class="info-box-content">
+                  <h4><b>SELAMAT DATANG</b></h4>
+                  <h6>Dashboard Pengelolaan Prospek Sales Digital Service</h6>
+                  <h7>merupakan aplikasi dashboard berbasis web yang menyediakan informasi mengenai pelanggan yang tidak berlangganan layanan digital</h7>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+      
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>10<sup style="font-size: 20px">%</sup></h3>
+
+              <p>MOVIN</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a class="small-box-footer">Jumlah Data Prospek Movin</a>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>20<sup style="font-size: 20px">%</sup></h3>
+
+              <p>INDIHOME GAMER</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a class="small-box-footer">Jumlah Data IH Gamer</a>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>30<sup style="font-size: 20px">%</sup></h3>
+
+              <p>OTT</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a class="small-box-footer">Jumlah Data Prospek OTT</a>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-maroon">
+            <div class="inner">
+              <h3>50<sup style="font-size: 20px">%</sup></h3>
+
+              <p>INDIHOME MUSIC</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a class="small-box-footer">Jumlah Data Prospek IH MUSIC</a>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>10<sup style="font-size: 20px">%</sup></h3>
+
+              <p>VIDEO CALL</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a class="small-box-footer">Jumlah Data Prospek Video Call</a>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>20<sup style="font-size: 20px">%</sup></h3>
+
+              <p>INDI STORAGE</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a class="small-box-footer">Jumlah Data Prospek Indi Storage</a>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>30<sup style="font-size: 20px">%</sup></h3>
+
+              <p>INDI SERVER</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a class="small-box-footer">Jumlah Data Prospek Indi Server</a>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-maroon">
+            <div class="inner">
+              <h3>50<sup style="font-size: 20px">%</sup></h3>
+
+              <p>PLC</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a class="small-box-footer">Jumlah Data Prospek PLC</a>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>10<sup style="font-size: 20px">%</sup></h3>
+
+              <p>WIFI EXTENDER</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a class="small-box-footer">Jumlah Data Prospek Wifi Extender</a>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>
+                20<sup style="font-size: 20px">%</sup>
+              </h3>
+
+              <p>INDIHOME SMART</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a class="small-box-footer">Jumlah Data Prospek IH SMART</a>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>30<sup style="font-size: 20px">%</sup></h3>
+
+              <p>INDIHOME STUDY</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a class="small-box-footer">Jumlah Data Prospek IH Study</a>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+
+      </div>
+      <!-- /.row -->
+      <!-- Main row -->
+      
+      <!-- /.row (main row) -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  </body>
+</html>
